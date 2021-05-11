1f8200c93edd56550b41a145c9957bc64e13611c
Ian Morland <imorland@users.noreply.github.com>
2021-05-11 14:58:14 +0000

    Apply fixes from StyleCI

--- src/Providers/LinkManipulatorProvider.php
+++ src/Providers/LinkManipulatorProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of fof/amazon-affiliation.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FoF\AmazonAffiliation\Providers;
 
 use Flarum\Foundation\AbstractServiceProvider;
