27e9ec2595ff9b499a24ac744799e0069f088e01
Ian Morland <ian@morland.me>
2021-05-26 09:00:33 +0100

    Flarum 1.0 (#13)
    * Add affilliate support to textformatter
    
    * Apply fixes from StyleCI
    
    * Add helptext around unsuppored MediaEmbed Amazon sites
    
    Co-authored-by: Ian Morland <imorland@users.noreply.github.com>

--- extend.php
+++ extend.php
@@ -12,6 +12,9 @@
 namespace FoF\AmazonAffiliation;
 
 use Flarum\Extend;
+use Flarum\Settings\Event\Saving as SettingsSaving;
+use Flarum\Settings\SettingsRepositoryInterface;
+use s9e\TextFormatter\Configurator;
 
 return [
     (new Extend\Frontend('admin'))
@@ -24,4 +27,41 @@ return [
 
     (new Extend\ServiceProvider())
         ->register(Providers\LinkManipulatorProvider::class),
+
+    (new Extend\Event())
+        ->listen(SettingsSaving::class, function (SettingsSaving $event) {
+            foreach ($event->settings as $key => $setting) {
+                if (strpos($key, 'fof-amazon-affiliation.affiliate-tag.') === 0) {
+                    resolve('flarum.formatter')->flush();
+
+                    return;
+                }
+            }
+        }),
+
+    (new Extend\Formatter())
+        ->configure(function (Configurator $configurator): void {
+            /** @var SettingsRepositoryInterface */
+            $settings = resolve(SettingsRepositoryInterface::class);
+            $prefix = 'fof-amazon-affiliation.affiliate-tag.';
+
+            $params = $configurator->rendering->parameters;
+            $params['AMAZON_ASSOCIATE_TAG'] = $settings->get($prefix.'com', '');
+            $params['AMAZON_ASSOCIATE_TAG_UK'] = $settings->get($prefix.'co.uk', '');
+            $params['AMAZON_ASSOCIATE_TAG_DE'] = $settings->get($prefix.'de', '');
+            $params['AMAZON_ASSOCIATE_TAG_FR'] = $settings->get($prefix.'fr', '');
+            $params['AMAZON_ASSOCIATE_TAG_JP'] = $settings->get($prefix.'co.jp', '');
+            $params['AMAZON_ASSOCIATE_TAG_CA'] = $settings->get($prefix.'ca', '');
+            $params['AMAZON_ASSOCIATE_TAG_IT'] = $settings->get($prefix.'it', '');
+            $params['AMAZON_ASSOCIATE_TAG_ES'] = $settings->get($prefix.'es', '');
+            $params['AMAZON_ASSOCIATE_TAG_IN'] = $settings->get($prefix.'in', '');
+
+            // Not currently supported by MediaEmbed. Will look to PR these into s9e/textformatter soon
+            // $params['AMAZON_ASSOCIATE_TAG_CN'] = $settings->get($prefix.'cn', '');
+            // $params['AMAZON_ASSOCIATE_TAG_BR'] = $settings->get($prefix.'com.br', '');
+            // $params['AMAZON_ASSOCIATE_TAG_MX'] = $settings->get($prefix.'com.mx', '');
+            // $params['AMAZON_ASSOCIATE_TAG_AU'] = $settings->get($prefix.'com.au', '');
+
+            extract($configurator->finalize());
+        }),
 ];
